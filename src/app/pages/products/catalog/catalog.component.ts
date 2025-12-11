cd6ceacc08e8d5131be940691107ea5e37f041e0
kuroko <dmillergg@gmail.com>
2025-12-11 16:01:02 -0500

    feat(catalog): skeleton product catalog

--- src/app/pages/products/catalog/catalog.component.ts
+++ src/app/pages/products/catalog/catalog.component.ts
@@ -12,11 +12,12 @@ import { ProductCardComponent } from '../components/product-card/product-card.co
 import { PaginatorComponent } from '@app/shared/components/paginator/paginator.component';
 import { AccordionComponent } from '@app/shared/components/accordion/accordion.component';
 import { CategorysPricePipe } from '@app/core/pipes/categorys-price.pipe';
+import { ProductSkeletonComponent } from '../components/product-skeleton/product-skeleton.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, DropdownComponent, SortPipe, SearchPipe, ProductCardComponent, PaginatorComponent, AccordionComponent, CategorysPricePipe],
+  imports: [ReactiveFormsModule, CommonModule, DropdownComponent, SortPipe, SearchPipe, ProductCardComponent, PaginatorComponent, AccordionComponent, ProductSkeletonComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
   providers: [ProductsService, CategorysPricePipe]
@@ -48,48 +49,55 @@ export class CatalogComponent implements OnInit {
   protected maxPrice: number = 0;
   protected minPrice: number = 0;
   protected filter: { categories: string[], currentPrice: number } = { categories: [], currentPrice: 0 };
+  protected loading: boolean = false;
 
   ngOnInit(): void {
+
     this.getProducts()
+
+
   }
 
   private getProducts() {
-  this.productService.getProducts().pipe(
-    take(1),
-    mergeMap((response) => {
-      this.catalogAll = response;
-      this.setPage(1);
+    this.loading = true;
+    setTimeout(() => {
+      this.productService.getProducts().pipe(
+        take(1),
+        mergeMap((response) => {
+          this.catalogAll = response;
+          this.setPage(1);
 
-      const counts = response.reduce((acc: Record<string, number>, product: any) => {
-        acc[product.category] = (acc[product.category] || 0) + 1;
-        return acc;
-      }, {});
-      this.categories = Object.keys(counts).map(cat => ({
-        name: cat,
-        value: cat,
-        checked: false,
-        cant: counts[cat]
-      }));
+          const counts = response.reduce((acc: Record<string, number>, product: any) => {
+            acc[product.category] = (acc[product.category] || 0) + 1;
+            return acc;
+          }, {});
+          this.categories = Object.keys(counts).map(cat => ({
+            name: cat,
+            value: cat,
+            checked: false,
+            cant: counts[cat]
+          }));
 
-      const prices = response.map((p: any) => p.price);
-      this.maxPrice = Math.max(...prices);
-      this.minPrice = Math.min(...prices);
+          const prices = response.map((p: any) => p.price);
+          this.maxPrice = Math.max(...prices);
+          this.minPrice = Math.min(...prices);
 
-      const detailRequests = response.map(p =>
-        this.productService.getSingleProduct(p.id).pipe(take(1))
-      );
+          const detailRequests = response.map(p =>
+            this.productService.getSingleProduct(p.id).pipe(take(1))
+          );
 
-      return forkJoin(detailRequests);
-    })
-  ).subscribe({
-    next: (fullProducts: Product[]) => {
-      this.catalog = fullProducts;
-      this.catalogAll = fullProducts;
-      this.setPage(1)
-      console.log('Productos detallados:', this.catalog);
-    }
-  });
-}
+          return forkJoin(detailRequests);
+        })
+      ).subscribe({
+        next: (fullProducts: Product[]) => {
+          this.loading = false;
+          this.catalog = fullProducts;
+          this.catalogAll = fullProducts;
+          this.setPage(1)
+        }
+      });
+    }, 5000);
+  }
 
 
   setPage(page: number) {
@@ -103,8 +111,8 @@ export class CatalogComponent implements OnInit {
     this.products = this.catalog.slice(start, end);
   }
 
-  protected applyFilter(){
-    this.catalog =this.categoryPricePipe.transform(this.catalogAll,this.filter);
+  protected applyFilter() {
+    this.catalog = this.categoryPricePipe.transform(this.catalogAll, this.filter);
     this.setPage(1)
   }
 }
